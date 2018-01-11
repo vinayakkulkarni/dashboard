3e08d1b92b5453bbe45ae547e1fa8fc0435116c4
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2018-01-11 17:34:52 +0530

    :vulcan_salute: [MAJOR] Playing around with Chart.js (WIP)

---
+++ src/components/Charts/Income.vue
@@ -0,0 +1,44 @@
+<script>
+import { Doughnut } from 'vue-chartjs';
+
+export default {
+  name: 'Income',
+  extends: Doughnut,
+  props: {
+    expenditure: {
+      type: Array,
+      required: true,
+    },
+    income: {
+      type: Array,
+      required: true,
+    },
+    locality: {
+      type: Object,
+      required: true,
+    },
+    pincode: {
+      type: Object,
+      required: true,
+    },
+  },
+  mounted() {
+    const t = this;
+    const options = {
+      responsive: true,
+      maintainAspectRatio: false,
+    };
+    const data = {
+      labels: ['income'],
+      datasets: [
+        {
+          fill: false,
+          backgroundColor: ['#A6D9EC', '#47A8DD'],
+          data: [t.income[0].income, t.income[1].income],
+        },
+      ],
+    };
+    this.renderChart(data, options);
+  },
+};
+</script>
