b45e0bbcec5f57f74d3919305dd316fb6402602e
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2018-01-15 22:14:39 +0530

    Registered global component

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import Vue2Leaflet from 'vue2-leaflet';
+import 'vue-awesome/icons';
+import Icon from 'vue-awesome/components/Icon';
 import App from './App';
 import router from './router';
 import '../node_modules/leaflet/dist/leaflet.css';
 
+Vue.component('icon', Icon);
 Vue.component('v-map', Vue2Leaflet.Map);
 Vue.component('v-tilelayer', Vue2Leaflet.TileLayer);
 Vue.component('v-marker', Vue2Leaflet.Marker);
